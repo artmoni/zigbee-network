becfef9343d323cdb558f28207f5f77dd4361d4f
PACHECO Luc <pachecoluc@mbp-de-admin-8.edu.sir>
2019-04-01 15:55:31 +0200

    add main method

--- src/zigbee/Main.java
+++ src/zigbee/Main.java
@@ -1,5 +1,26 @@
 package zigbee;
 
-public class Main {
+import com.rapplogic.xbee.api.XBee;
+import com.rapplogic.xbee.api.XBeeException;
+import com.rapplogic.xbee.api.wpan.RxResponseIoSample;
 
+public class Main {
+	static String MY_PORT = "/dev/ttyUSB0"; 
+	static XBee xbee = new XBee();
+	
+	public static void main (String[] args) throws XBeeException{
+
+		xbee.open(MY_PORT, 9600);
+		System.out.println("test");
+		
+		while (true) {
+		    RxResponseIoSample ioSample = (RxResponseIoSample) xbee.getResponse();
+
+		    System.out.println("We received a sample from " + ioSample.getSourceAddress());	
+					
+		    if (ioSample.containsAnalog()) {
+		        System.out.println("10-bit temp reading (pin 19) is " + ioSample.getSamples()[0].getAnalog1());
+		    }
+		}
+	}
 }
