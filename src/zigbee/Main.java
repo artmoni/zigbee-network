3ef47b4f022d2216c0dec87135d94ad0a75db533
KAHLAOUI Mathieu <mathieu.kahlaoui@oscaro.com>
2019-04-02 15:24:12 +0200

    wip znet function

--- src/zigbee/Main.java
+++ src/zigbee/Main.java
@@ -1,8 +1,11 @@
 package zigbee;
 
+import com.rapplogic.xbee.api.ApiId;
 import com.rapplogic.xbee.api.XBee;
 import com.rapplogic.xbee.api.XBeeException;
+import com.rapplogic.xbee.api.XBeeResponse;
 import com.rapplogic.xbee.api.wpan.RxResponseIoSample;
+import com.rapplogic.xbee.api.zigbee.ZNetRxIoSampleResponse;
 
 public class Main {
 	static String MY_PORT = "/dev/ttyUSB0"; 
@@ -11,16 +14,21 @@ public class Main {
 	public static void main (String[] args) throws XBeeException{
 
 		xbee.open(MY_PORT, 9600);
-		System.out.println("test");
 		
-		while (true) {
-		    RxResponseIoSample ioSample = (RxResponseIoSample) xbee.getResponse();
+		XBeeResponse response = xbee.getResponse();
 
-		    System.out.println("We received a sample from " + ioSample.getSourceAddress());	
-					
-		    if (ioSample.containsAnalog()) {
-		        System.out.println("10-bit temp reading (pin 19) is " + ioSample.getSamples()[0].getAnalog1());
-		    }
+		while (true) {
+			if (response.getApiId() == ApiId.ZNET_IO_SAMPLE_RESPONSE) {
+				ZNetRxIoSampleResponse ioSample = (ZNetRxIoSampleResponse) response;
+				System.out.println("RESPONSE "+ ioSample);
+				System.out.println("Received a sample from " + ioSample.getRemoteAddress64());
+				
+
+				System.out.println("Analog D0 (pin 20) 10-bit reading is " + ioSample.getAnalog0());
+				System.out.println("Digital D4 (pin 11) is " + (ioSample.isD4On() ? "on" : "off"));
+			}
 		}
 	}
+	
+	
 }
